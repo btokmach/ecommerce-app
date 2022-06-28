565276b81c9b992f246a18c957bc92d71e5b7600
btokmach <btokmach@gmail.com>
2022-06-29 01:26:35 +0300

    cart logic implemented

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -9,7 +9,7 @@ import { urlFor } from '../lib/client';
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart } = useStateContext();
+  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemove } = useStateContext();
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -50,15 +50,15 @@ const Cart = () => {
                 <div className="flex bottom">
                   <div>
                   <p className="quantity-desc">
-                    <span className="minus" onClick=""><AiOutlineMinus /></span>
-                    <span className="num" onClick="">0</span>
-                    <span className="plus" onClick=""><AiOutlinePlus /></span>
+                    <span className="minus" onClick={() => toggleCartItemQuantity(item._id, 'dec')}><AiOutlineMinus /></span>
+                    <span className="num" onClick="">{item.quantity}</span>
+                    <span className="plus" onClick={() => toggleCartItemQuantity(item._id, 'inc')}><AiOutlinePlus /></span>
                   </p>
                   </div>
                   <button
                     type="button"
                     className="remove-item"
-                    onClick=""
+                    onClick={() => onRemove(item)}
                   >
                     <TiDeleteOutline />
                   </button>
